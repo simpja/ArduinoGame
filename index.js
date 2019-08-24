3e9bf0fb59988320c0481c9ceaa1ead61fc0a2ee
simpja <simenpjaat@gmail.com>
2019-08-24 22:25:36 +0200

    posting the data to localhost

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-var five = require("johnny-five");
+var five = require("johnny-five"),
+  board,
+  potentiometer;
+var http = require("http");
+var fs = require("fs");
 var board = new five.Board();
+var potentionmeterData;
+
+http
+  .createServer(function(req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("hello world: " + potentionmeterData.toString());
+    res.end();
+  })
+  .listen(8080);
 
 board.on("ready", function() {
-  // Create a standard 'Led' component instance
-  var led = new five.Led(13);
+  // Create a new 'potentiometer' hardware instance
+  potentiometer = new five.Sensor({
+    pin: "A3",
+    freq: 250
+  });
 
-  // "blink" the led in 500ms
-  // on-off phase periods
-  led.blink(500);
+  // Inject the 'sensor' hardware into
+  // the repl instance's context;
+  // allows direct command line access
+  board.repl.inject({
+    pot: potentiometer
+  });
+
+  // "data" get the current reading from the potentiometer
+  potentiometer.on("data", function() {
+    console.log(this.value);
+    potentionmeterData = this.value;
+  });
 });
