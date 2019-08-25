7db4aa96b7d4d4c73a3e7abe3ce7fe1d6d67a45b
Rikard Eide <rix1@otovo.com>
2019-08-25 20:59:30 +0200

    example of passing value through html document

--- index.js
+++ index.js
@@ -4,12 +4,28 @@ var five = require("johnny-five"),
 var http = require("http");
 var fs = require("fs");
 var board = new five.Board();
-var potentionmeterData;
+var potentionmeterData = 12;
 
 http
   .createServer(function(req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("hello world: " + potentionmeterData.toString());
+    // const html = renderToString(
+    //   <GameApp controllerValue={potentionmeterData} />
+    // ); // react
+    res.write(`
+    <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>ArduinoGame</title>
+      </head>
+      <body>
+       <h1>Hi! This is the value: ${potentionmeterData}</h1> 
+      </body>
+      </html>
+    `);
     res.end();
   })
   .listen(8080);
