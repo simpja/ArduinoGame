6bd0b69d0c77843b8f7b4bb3acb5574bc119e68e
Rikard Eide <rix1@otovo.com>
2019-08-25 21:34:43 +0200

    fetch potentionmeter value from client

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ var potentionmeterData = 12;
 
 http
   .createServer(function(req, res) {
+    console.log("req", req.url);
+    if (req.url === "/data") {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.write(JSON.stringify({ potentionmeterData }));
+      res.end(); //end the response
+      return;
+    }
+
     res.writeHead(200, { "Content-Type": "text/html" });
     // const html = renderToString(
     //   <GameApp controllerValue={potentionmeterData} />
@@ -15,15 +23,39 @@ http
     res.write(`
     <!DOCTYPE html>
       <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>ArduinoGame</title>
-      </head>
-      <body>
-       <h1>Hi! This is the value: ${potentionmeterData}</h1> 
-      </body>
+        <head>
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+          <title>Http Fun on a friday</title>
+        </head>
+
+        <body>
+          <h1 id="data-placeholder">Ingenting</h1>
+          <button onclick="postMessage('UP')">UP</button>
+          <button onclick="postMessage('DOWN')">DOWN</button>
+        </body>
+
+        <script>
+          async function postMessage(direction) {
+            const API_ENDPOINT = "http://localhost:8080/data";
+
+            const response = await fetch(API_ENDPOINT, {
+              method: "GET",
+              headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+              }
+            });
+
+            const json = await response.json();
+
+            document.getElementById(
+              "data-placeholder"
+            ).innerHTML = "New Value: " + json.potentionmeterData;
+            console.log(json);
+          }
+        </script>
       </html>
     `);
     res.end();
